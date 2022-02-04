6b2aeaf2afb484bea2ac10d2343e855bec3ed51a
FILIPE AZEVEDO DA ROCHA <filipe.azevedo@grupomoura.com>
2022-02-04 15:24:46 -0300

    Adicionando OtherEntities

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace SalesWebMvc.Models
 {
     public class Department
@@ -10,5 +11,30 @@ namespace SalesWebMvc.Models
         public int Id   { get; set; }
         public string Name { get; set; }
 
+        public int MyProperty { get; set; }
+
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+
+        public Department() 
+        { 
+        }
+
+        public Department(int id, string name, int myProperty)
+        {
+            Id = id;
+            Name = name;
+            MyProperty = myProperty;
+        }
+
+        public void AddSeller(Seller seller) 
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSalles (DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
